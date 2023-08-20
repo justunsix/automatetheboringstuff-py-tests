12b0ae62ac1be8efc1e6a507b2024778e299dbee
Justin Tung <justin.tung@live.ca>
2023-08-20 11:21:06 -0400

    learning basics

--- src/learn/devops-python-basics.py
+++ src/learn/devops-python-basics.py
@@ -1,5 +1,9 @@
-# Learning Basics of Python:
-# String/number data types, variables, functions, parameters
+# DevOps: Learning Basics of Python
+# with https://www.techworld-with-nana.com/devops-bootcamp
+
+##############################################################
+# String/number data types, variables, functions, parameters #
+##############################################################
 
 # Number of units in a day
 # Integer variable, global scope variable
@@ -31,3 +35,30 @@ days_to_units(20, "Awesome!")
 days_to_units(110, "Nice!")
 
 scope_check(54)
+
+#########
+# Input #
+#########
+
+
+# Return number of units in days as string
+def days_to_units_text(num_of_days):
+    return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
+
+# Ask users for input and store user's input in a variable
+# The input function always returns a string variable
+user_input = input("Input a number of days and I will convert it to seconds\n")
+print(user_input)
+
+# To use the user_input as a number
+# use casting, call the int function to convert the string to an integer
+user_input_number = int(user_input)
+
+calculated_value = days_to_units_text(user_input_number)
+print(calculated_value)
+
+
+###################################################
+# Conditionals (if / else) and Boolean Data Type  #
+###################################################
+
