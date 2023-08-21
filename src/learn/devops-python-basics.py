93242104e7c9f11e681bf7f91cf170ea61699896
Justin Tung <justin.tung@live.ca>
2023-08-20 23:29:25 -0400

    python learning

--- src/learn/devops-python-basics.py
+++ src/learn/devops-python-basics.py
@@ -60,5 +60,80 @@ print(calculated_value)
 
 ###################################################
 # Conditionals (if / else) and Boolean Data Type  #
+# Input validation                                #
 ###################################################
 
+
+# Return number of units in days as string
+# Validate input is a positive number
+def days_to_units_text_checked(num_of_days):
+    # Check if parameter is a positive number
+    # assumes num_of_days is an integer data type
+    condition_check = num_of_days > 0
+    # Print the data type of the condition_check variable
+    print(type(condition_check))
+    # Conditional to check if parameter is a positive number and evaluates to a boolean
+    # true: parameter is a positive number
+    # false: parameter is not a positive number
+    if num_of_days > 0:
+        return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
+    elif num_of_days == 0:
+        return "You entered a 0. Enter a positive number"
+    else:
+        return "You entered a negative or other data type. Enter a positive number."
+
+
+# Validate input is an integer
+# isdigit() method returns True if all characters in a string are digits
+if user_input.isdigit():
+    user_input_number = int(user_input)
+    checked_calculated_value = days_to_units_text_checked(user_input_number)
+    print(checked_calculated_value)
+else:
+    print("Your input is not a valid number. Enter a positive number.")
+
+##################
+# Cleaned up Code
+
+
+def validate_and_execute():
+    if user_input.isdigit():
+        user_input_number = int(user_input)
+        if user_input_number > 0:
+            checked_calculated_value = days_to_units_text_validated(user_input_number)
+            print(checked_calculated_value)
+        elif user_input_number == 0:
+            return "You entered a 0. Enter a positive number"
+    else:
+        print("Your input is not a valid number. Enter a positive number.")
+
+
+# Return number of units in days as string
+# Parameter num_of_days must be a postive integer
+def days_to_units_text_validated(num_of_days):
+    return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
+
+
+validate_and_execute()
+
+##################################
+# Error Handling with Try-Except #
+##################################
+
+
+def validate_and_execute_tried():
+    try:
+        if user_input.isdigit():
+            user_input_number = int(user_input)
+            if user_input_number > 0:
+                checked_calculated_value = days_to_units_text_validated(user_input_number)
+                print(checked_calculated_value)
+            elif user_input_number == 0:
+                return "You entered a 0. Enter a positive number"
+            else:
+                return("You entered a negative number. Enter a positive number.")
+    except ValueError:
+        print("Error: Your input is not a valid number.")
+
+
+validate_and_execute_tried()
