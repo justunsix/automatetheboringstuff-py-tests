afccf7c6206dd85206f9e422470ec66c041d8536
Justin Tung <justin.tung@live.ca>
2024-04-19 14:42:04 -0400

    externalize variables to config file, allow multiple destinations

--- src/project/kubernetes/k8s-oc-test-pod-connections.py
+++ src/project/kubernetes/k8s-oc-test-pod-connections.py
@@ -4,15 +4,10 @@
 
 # Requires openshift-client installed per https://github.com/openshift/openshift-client-python
 import openshift as oc
+import k8soctestpodconnectionsconfig as config
 
-# Project names
-OC_PROJECT_SELECTED = 'selected-project'
-
-DESTINATION_IP = '10.10.2.3'
-DESTINATION_PORT = '1500'
-
-# Output help
-NC_COMMAND = f'nc -zv {DESTINATION_IP} {DESTINATION_PORT}'
+OC_PROJECT_SELECTED = config.OC_PROJECT_SELECTED
+DESTINATIONS = config.DESTINATIONS
 
 # print('OpenShift client version: {}'.format(oc.get_client_version()))
 
@@ -34,12 +29,18 @@ with (oc.project(OC_PROJECT_SELECTED), oc.timeout(30)):
         result_date = pod_obj.execute(['date'])
         print(f'Date (UTC): {result_date.out()}')
         # Run network test on pod to run netcat on container
-        try: 
-            result_nc = pod_obj.execute(['nc', '-zv', DESTINATION_IP, DESTINATION_PORT])
-            # if result_nc.err() contains string Connected, print success
-            if 'Connected' in result_nc.err():
-                print(f"✓ Connection was successful from pod running {NC_COMMAND}")
-                print(f"nc output: {result_nc.err()}")
-        except Exception as e:
-            print(f'X Connection was NOT successful from pod running {NC_COMMAND}:')
-            print(e)
+        for ip, port in DESTINATIONS.items():
+            destination_ip = ip
+            destination_port = port              
+            # Output help
+            nc_command = f'nc -zv {destination_ip} {destination_port}'
+        
+            try: 
+                result_nc = pod_obj.execute(['nc', '-zv', destination_ip, destination_port])
+                # if result_nc.err() contains string Connected, print success
+                if 'Connected' in result_nc.err():
+                    print(f"✓ Connection was successful from pod running {nc_command}")
+                    print(f"nc output: {result_nc.err()}")
+            except Exception as e:
+                print(f'X Connection was NOT successful from pod running {nc_command}:')
+                print(e)
---
+++ src/project/kubernetes/k8soctestpodconnectionsconfig.py
@@ -0,0 +1,5 @@
+# Project names
+OC_PROJECT_SELECTED = 'selected-project'
+
+# IP and port destinations to test
+DESTINATIONS = { '10.10.2.3': '1433', '10.24.5.3': '3389' }
