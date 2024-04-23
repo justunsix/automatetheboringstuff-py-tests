7e5395221726a0912354c5b70935579f9defa1fd
Justin Tung <justin.tung@live.ca>
2024-04-23 13:27:00 -0400

    config file format

--- src/project/kubernetes/k8soctestpodconnectionsconfig.py
+++ src/project/kubernetes/k8soctestpodconnectionsconfig.py
@@ -2,4 +2,7 @@
 OC_PROJECT_SELECTED = 'selected-project'
 
 # IP and port destinations to test
-DESTINATIONS = { '10.10.2.3': '1433', '10.24.5.3': '3389' }
+DESTINATIONS = { 
+                '10.10.2.3': '1433', 
+                '10.24.5.3': '3389' 
+                }
