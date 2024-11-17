93eb426fccf83b472a8c1bcf4a867cea9d562131
Justin Tung <justin.tung@live.ca>
2024-11-16 21:11:29 -0500

    optimize: renamed file variable to avoid similarity with arg

--- src/project/csv/csv-get-rows-from-values-in-other-csv.py
+++ src/project/csv/csv-get-rows-from-values-in-other-csv.py
@@ -1,6 +1,6 @@
 """
 This script extracts values from a specified column in one CSV file called `csv_with_values_to_be_searched`
-that match a certain filter value, and then searches for rows in another CSV file called 
+that match a certain filter value, and then searches for rows in another CSV file called
 `csv_to_be_filtered` that contain any of these extracted values.
 The matching rows are written to a new CSV file called `csv_results_file`.
 
@@ -33,8 +33,8 @@ def get_values_from_csv(csvfile, values_from_csv):
         csvfile (str): The path to the CSV file to be read.
         values_from_csv (list): The list to store the extracted values.
     """
-    with open(csvfile, newline="") as csvfile:
-        reader = csv.DictReader(csvfile)
+    with open(csvfile, newline="") as file:
+        reader = csv.DictReader(file)
         for row in reader:
             if COLUMN_VALUE_FILTER in row[COLUMN_NAME_FILTER]:
                 values_from_csv.append(row[COLUMN_NAME_FILTER])
