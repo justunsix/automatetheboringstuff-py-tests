d55cfbd10572549791643dd543d74679b65cb577
Justin Tung <justin.tung@live.ca>
2025-01-20 20:05:33 -0500

    feat(project): spreadsheet initial commit

---
+++ src/project/spreadsheet/README.md
@@ -0,0 +1,3 @@
+# Spreadsheet
+
+Common use cases to replace use of graphical spreadsheet programs.
---
+++ src/project/spreadsheet/polars-getting-started.py
@@ -0,0 +1,44 @@
+import polars as pl
+import datetime as dt
+
+# Example data
+df = pl.DataFrame(
+    {
+        "name": ["Alice Archer", "Ben Brown", "Chloe Cooper", "Daniel Donovan"],
+        "birthdate": [
+            dt.date(1997, 1, 10),
+            dt.date(1985, 2, 15),
+            dt.date(1983, 3, 22),
+            dt.date(1981, 4, 30),
+        ],
+        "weight": [57.9, 72.5, 53.6, 83.1],  # (kg)
+        "height": [1.56, 1.77, 1.65, 1.75],  # (m)
+    }
+)
+
+print(df)
+
+# Write to csv
+df.write_csv("output.csv")
+
+# Read a csv
+df_csv = pl.read_csv("output.csv", try_parse_dates=True)
+print(df_csv)
+
+# Select certain columns
+# Rename a column
+# Calculate Body Mass Index using other columns
+result = df.select(
+    pl.col("name"),
+    pl.col("birthdate").dt.year().alias("birth_year"),
+    (pl.col("weight") / (pl.col("height") ** 2)).alias("bmi"),
+)
+print(result)
+
+# Use "expression expansion" to change multiple columns
+# Add a suffix to the name of the multiple columns
+result = df.select(
+    pl.col("name"),
+    (pl.col("weight", "height") * 0.95).round(2).name.suffix("-5%"),
+)
+print(result)
