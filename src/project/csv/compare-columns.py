49e3561a0bc391596acda53a3c0c0b96fd32c566
Justin Tung <justin.tung@live.ca>
2025-05-24 23:10:16 -0400

    fix(csv): email col, format

--- src/project/csv/compare-columns.py
+++ src/project/csv/compare-columns.py
@@ -12,7 +12,8 @@ import csv
 file1_location = "examples/users.csv"
 file1_column_to_read = "email"
 file2_location = "examples/users-db.csv"
-file2_column_to_read = "Email"
+file2_column_to_read = "email"
+
 
 # Compare values from the 2 files and same and differences in values
 def compare_columns(col1, col2):
@@ -25,10 +26,11 @@ def compare_columns(col1, col2):
 
     return same_values, only_in_set1, only_in_set2
 
+
 # Read a column from a CSV file and store in a list
 def read_column_from_file(file_location, column_to_read):
     col_values = []
-    with open(file_location, newline='') as csvfile:
+    with open(file_location, newline="") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             # add row's column value and it make a lower case and trim extra whitespace, then append to list
@@ -37,10 +39,13 @@ def read_column_from_file(file_location, column_to_read):
             col_values.append(row_value)
     return col_values
 
+
 file1_col_values = read_column_from_file(file1_location, file1_column_to_read)
 file2_col_values = read_column_from_file(file2_location, file2_column_to_read)
 
-same_values, only_in_set1, only_in_set2 = compare_columns(file1_col_values, file2_col_values)
+same_values, only_in_set1, only_in_set2 = compare_columns(
+    file1_col_values, file2_col_values
+)
 
 print("Values in both files:", same_values)
 print("##########################################################################")
@@ -50,3 +55,4 @@ print("#########################################################################
 print("Values only in file 1:", only_in_set1)
 print("##########################################################################")
 print("Values only in file 2:", only_in_set2)
+
